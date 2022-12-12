159139c8f41caa95384dae426bfe0559283f3410
mike.poret <mike.poret@sunrun.com>
2022-12-12 12:12:03 -0700

    Bug fixes for launching ControlActivity

--- app/src/main/java/com/sunrun/smartprompt/ControlActivity.java
+++ app/src/main/java/com/sunrun/smartprompt/ControlActivity.java
@@ -27,29 +27,37 @@ public class ControlActivity extends AppCompatActivity {
 
         script_container = findViewById(R.id.txt_script_entry);
         Intent intent = getIntent();
-        String scriptText = "SD";
-        scriptText = intent.getStringExtra(Intent.EXTRA_TEXT);
 
-        //App sent plain text in the intent
-        if (scriptText != null) {
-            script_container.setText(scriptText);
-        }
-        //Google docs returns a "content://" Uri
-        else{
-            //Try to extract data from uri content
-            InputStream inputStream = null;
-            try {
-                Uri uri = (Uri) intent.getExtras().get("android.intent.extra.STREAM");
-                ContentResolver resolver = getContentResolver();
-                inputStream = resolver.openInputStream(uri);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+        //Only extract Text if this came from a send
+        boolean script_import;
+        String action = intent.getAction();
+        script_import = action != null && action.contains("action.SEND");
+
+        if(script_import) {
+
+            String scriptText;
+            scriptText = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+            //App sent plain text in the intent
+            if (scriptText != null) {
+                script_container.setText(scriptText);
             }
-            String text = convertStreamToString(inputStream);
-            Status.setScript(text);
-            script_container.setText(text);
+            //Google docs returns a "content://" Uri
+            else {
+                //Try to extract data from uri content
+                InputStream inputStream = null;
+                try {
+                    Uri uri = (Uri) intent.getExtras().get("android.intent.extra.STREAM");
+                    ContentResolver resolver = getContentResolver();
+                    inputStream = resolver.openInputStream(uri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                String text = convertStreamToString(inputStream);
+                Status.setScript(text);
+                script_container.setText(text);
+            }
         }
-
     }
 
     private String convertStreamToString(InputStream is) {
