ee1e1db855ac30ba8dcddb6cc97af463fe96bed4
mporetvivint <mike.poret@sunrun.com>
2022-12-15 09:34:37 -0700

    Implemented NearbyCom

--- app/src/main/java/com/sunrun/smartprompt/com/NearbyCom.java
+++ app/src/main/java/com/sunrun/smartprompt/com/NearbyCom.java
@@ -10,6 +10,10 @@ import com.google.android.gms.nearby.connection.AdvertisingOptions;
 import com.google.android.gms.nearby.connection.ConnectionInfo;
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
 import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
 import com.google.android.gms.nearby.connection.Strategy;
 
 public class NearbyCom {
@@ -17,14 +21,68 @@ public class NearbyCom {
     ConnectionLifecycleCallback advertiserCallback;
     ConnectionLifecycleCallback discovererCallback;
 
+
+
     public NearbyCom() {
 
-        //set up callback for advertiser
-        advertiserCallback = new ConnectionLifecycleCallback() {
+    }
+
+    private static void startAdvertising(Context context) {
+
+        ConnectionLifecycleCallback lifecycleCallback = new ConnectionLifecycleCallback() {
             @Override
-            public void onConnectionInitiated(@NonNull String s, @NonNull ConnectionInfo connectionInfo) {
+            public void onConnectionInitiated(@NonNull String endpointId, @NonNull ConnectionInfo connectionInfo) {
                 // Automatically accept the connection on both sides.
                 Nearby.getConnectionsClient(context).acceptConnection(endpointId, payloadCallback);
+            }
+
+            @Override
+            public void onConnectionResult(@NonNull String endpointId, @NonNull ConnectionResolution result) {
+                switch (result.getStatus().getStatusCode()) {
+                    case ConnectionsStatusCodes.STATUS_OK:
+                        // We're connected! Can now start sending and receiving data.
+                        break;
+                    case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                        // The connection was rejected by one or both sides.
+                        break;
+                    case ConnectionsStatusCodes.STATUS_ERROR:
+                        // The connection broke before it was able to be accepted.
+                        break;
+                    default:
+                        // Unknown status code
+                }
+            }
+
+            @Override
+            public void onDisconnected(@NonNull String s) {
+
+            }
+        };
+
+
+        AdvertisingOptions advertisingOptions =
+                new AdvertisingOptions.Builder().setStrategy(Strategy.P2P_STAR).build();
+
+
+        Nearby.getConnectionsClient(context)
+                .startAdvertising(
+                        "SmartPromptControl", "com.sunrun.smartprompt", lifecycleCallback, advertisingOptions)
+                .addOnSuccessListener(
+                        (Void unused) -> {
+                            // We're advertising!
+                        })
+                .addOnFailureListener(
+                        (Exception e) -> {
+                            // We were unable to start advertising.
+                        });
+    }
+
+    private static void startDiscovery(Context context){
+
+        ConnectionLifecycleCallback lifecycleCallback = new ConnectionLifecycleCallback() {
+            @Override
+            public void onConnectionInitiated(@NonNull String s, @NonNull ConnectionInfo connectionInfo) {
+
             }
 
             @Override
@@ -39,40 +97,40 @@ public class NearbyCom {
         };
 
         //setup callback for discoverer
-        discovererCallback = new ConnectionLifecycleCallback() {
+        EndpointDiscoveryCallback endpointDiscoveryCallback = new EndpointDiscoveryCallback() {
             @Override
-            public void onConnectionInitiated(@NonNull String s, @NonNull ConnectionInfo connectionInfo) {
-
+            public void onEndpointFound(@NonNull String s, @NonNull DiscoveredEndpointInfo discoveredEndpointInfo) {
+                // An endpoint was found. We request a connection to it.
+                Nearby.getConnectionsClient(context)
+                        .requestConnection("SmartPromptControl", "com.sunrun.smartprompt", lifecycleCallback)
+                        .addOnSuccessListener(
+                                (Void unused) -> {
+                                    // We successfully requested a connection. Now both sides
+                                    // must accept before the connection is established.
+                                })
+                        .addOnFailureListener(
+                                (Exception e) -> {
+                                    // Nearby Connections failed to request the connection.
+                                });
             }
 
             @Override
-            public void onConnectionResult(@NonNull String s, @NonNull ConnectionResolution connectionResolution) {
-
-            }
-
-            @Override
-            public void onDisconnected(@NonNull String s) {
+            public void onEndpointLost(@NonNull String s) {
 
             }
         };
 
-    }
-
-    private void startAdvertising(Context context) {
-        AdvertisingOptions advertisingOptions =
-                new AdvertisingOptions.Builder().setStrategy(Strategy.P2P_STAR).build();
-
-
+        DiscoveryOptions discoveryOptions =
+                new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_STAR).build();
         Nearby.getConnectionsClient(context)
-                .startAdvertising(
-                        "SmartPromptControl", "com.sunrun.smartprompt", advertiserCallback, advertisingOptions)
+                .startDiscovery("SmartPromptControl", endpointDiscoveryCallback, discoveryOptions)
                 .addOnSuccessListener(
                         (Void unused) -> {
-                            // We're advertising!
+                            // We're discovering!
                         })
                 .addOnFailureListener(
                         (Exception e) -> {
-                            // We were unable to start advertising.
+                            // We're unable to start discovering.
                         });
     }
 }
